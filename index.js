b44ae7f11b7bfaea8c97d1ed20026554ad774da3
Gian William Ramelb <144856637+rame0033@users.noreply.github.com>
2025-02-07 17:59:59 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// /* DESTRUCTURING
+// const contacts = [
+//     {
+//         name: 'Cody',
+//         phone: '123-456-7890',
+//         email: 'cody@sample.com'
+//     },
+//     {
+//         name: 'John',
+//         phone: '123-456-7890',
+//         email: 'john@sample.com',
+//     },
+//     {
+//         name: 'Jane',
+//         phone: '123-456-7890',
+//         email: 'jane@sample.com',
+//     },
+//     {
+//         name: 'Jill',
+//         phone: '123-456-7890',
+//         email: 'jill@sample.com',
+//     }
+// ]
+
+// /*
+// const contact1 = contacts [0];
+// console.log(contact1);
+// */
+
+// const [cody, john, jane] = contacts;
+// console.log(john); // { name: 'John', phone: '123-456-7890', email: '
+
+
+// const grades = {
+//     math: 'B-',
+//     science: 'A+',
+//     english: 'C',
+//     history: 'A',
+// }
+
+// const { math, science, english, history } = grades;
+// console.log(`My english grade was ${english} and my history grade was ${history}.`); // My math grade was B- and my science grade was A+.
+
+// const list = [
+//         {
+//             title: 'Post 1',
+//             content: 'This is post 1'
+//         },
+//         {
+//             title: 'Post 2',
+//             content: 'This is post 2'
+//         }
+//     ];
+
+//     const [comment1] = list;
+
+//     const  { content }  = comment1;
+//     console.log(content); // This is post 1
+
+// EVENT HANDLING
